1f817afb7244e98ac8483d59e66aa3f71b9563b2
kelsob <kelsob@gmail.com>
2025-02-03 12:52:52 -0500

    Initial commit. Laravel project setup with TALL stack libraries.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
